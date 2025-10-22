b653869ea3a4d5d82995f16924cf997a705988f9
Beo Dammy <beodammy@gmail.com>
2025-10-22 01:19:05 +0100

    second commit where i learnt how to use the 'println() method.'

--- Pro.java
+++ Pro.java
@@ -1,6 +1,8 @@
 public class Pro  {
 	public static void main(String[] args){
 		System.out.println("Hellow world!");
+		System.out.println("coding in Java is fun");
+		System.out.println("I know how to debug my code");
 	}
 }
 	
