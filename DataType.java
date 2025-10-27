ae51bc224b48ecf277cca073eb536a80c7ac4bcd
Beo Dammy <beodammy@gmail.com>
2025-10-27 07:32:04 +0100

    assignment on chapter one

--- DataType.java
+++ DataType.java
@@ -1,11 +1,11 @@
 public class DataType{
-	
-	
+
+
 	public static void main(String[] args){
 	byte age = 23;
 	short quantity = 20394;
 	int nigeria__population = 2000000000;
-	long worldspopulation = 321000000000000L; 
+	long worldspopulation = 321000000000000L;
 	float price  = 458.315F;
 	double balance = 3456926.29345862;
 	char symbol ='$';
@@ -17,7 +17,7 @@ public class DataType{
 		System.out.printf("The price for two books is %.2f %n",price);
 		System.out.printf("My account balance is %.2f%n",balance);
 		System.out.printf("My account balance is %c%f%.2f%n",symbol,balance);
-		
-		
+
+
 	}
 }
